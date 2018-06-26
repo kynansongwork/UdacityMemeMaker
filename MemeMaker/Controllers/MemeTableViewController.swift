39f510ae40b9e68abacf900a6c2ae6706c164410
Kynan Song <kynan.song@bemo.co>
2018-06-26 17:11:41 +0100

    delete function added on the table. Currently only a temporary action.

--- MemeMaker/Controllers/MemeTableViewController.swift
+++ MemeMaker/Controllers/MemeTableViewController.swift
@@ -12,6 +12,7 @@ class MemeTableViewController: UITableViewController {
 
 
     var memes: [Meme]!
+    var deleteMemeIndexPath: NSIndexPath? = nil
         
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -45,7 +46,50 @@ class MemeTableViewController: UITableViewController {
         tableDetailController.meme = self.memes[(indexPath as NSIndexPath).row]
         self.navigationController!.pushViewController(tableDetailController, animated: true)
     }
+    
+    
+    // Delete functionality
+    //Found on stackOverflow and https://www.andrewcbancroft.com/2015/07/16/uitableview-swipe-to-delete-workflow-in-swift/
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            deleteMemeIndexPath = indexPath as NSIndexPath
+            let memeToDelete = memes[indexPath.row]
+            confirmDelete(meme: memeToDelete)
+        }
+        //This will delete the data from array and will update the views.
+    }
+    
+    func confirmDelete(meme: Meme) {
+        let alert = UIAlertController(title: "Delete Meme", message: "Do you want to delete this meme?", preferredStyle: .actionSheet)
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handleDeleteMeme)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelDeleteMeme)
         
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
         
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func handleDeleteMeme(alertAction: UIAlertAction!) -> Void {
+        if let indexPath = deleteMemeIndexPath {
+            tableView.beginUpdates()
+            memes.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath as IndexPath], with: .automatic)
+            deleteMemeIndexPath = nil
+            tableView.endUpdates()
+        }
+    }
+    
+    func cancelDeleteMeme(alertAction: UIAlertAction!) {
+        deleteMemeIndexPath = nil
+    }
+    
+    //Currently only a temporary delete. Only deletes from present view but returns if the page is left.
 }
 
